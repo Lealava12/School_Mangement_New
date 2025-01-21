cdb3ab0eabe1a14f43401b83d26d232970c115d7
Lealava12 <technologieslealava@gmail.com>
2025-01-21 10:48:19 +0530

    updated code

--- src/index.js
+++ src/index.js
@@ -5,11 +5,8 @@ import Signup from "./Signup" ;
 import Signin from "./Signin";
 // import PythonBackend from './Python_backend';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
-=======
 // import Login from './Login';
 // import Signup from './Signup';
->>>>>>> 1ad0590655d88fb6f9399b7d864bfd3576b3e8e3
 import Sidebar from './Sidebar';
 import Teacherm from "./Teacherm";
 import Studentm from './Studentm'
@@ -24,16 +21,11 @@ root.render(
     <Router>
       <Routes>
         {/* <PythonBackend /> */}
-<<<<<<< HEAD
+
         <Route path="/" element={<Signup />} />
         <Route path="/Signup" element={<Signup />} />
-        <Route path="/Signin" element={<Signin/>} />
-        <Route path="/Signin" element={<Sidebar />} />
-=======
-        <Route path="/" element={<Sidebar />} />
-        <Route path="/Signup" element={<Sidebar />} />
+        <Route path="/Signin" element={<Signin />} />
           {/* <Route path="/Login" element={<Login />} /> */}
->>>>>>> 1ad0590655d88fb6f9399b7d864bfd3576b3e8e3
         <Route path="/Teacherm" element={<Teacherm />} />
         <Route path="/Studentm" element={<Studentm />} />
         <Route path="/Fee" element={<Fee />} />
